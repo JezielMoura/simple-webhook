a33fe46ebea0c66cebac6cebfd79d5240b6033e3
Jeziel Moura <jezielmoura@outlook.com>
2025-08-15 13:54:50 -0300

    Add ip

--- Program.cs
+++ Program.cs
@@ -1,21 +1,13 @@
-using System.Text;
-using System.Text.Json.Serialization;
-
 var builder = WebApplication.CreateSlimBuilder(args);
 var callbacks = new List<Callback>();
-
-builder.Services.ConfigureHttpJsonOptions(options =>
-{
-    options.SerializerOptions.TypeInfoResolverChain.Insert(0, AppJsonSerializerContext.Default);
-});
-
 var app = builder.Build();
 
 app.MapPost("/callbacks", async (HttpRequest request) =>
 {
     using var reader = new StreamReader(request.Body);
     var content = await reader.ReadToEndAsync();
-    callbacks.Add(new(Guid.NewGuid(), content, DateTime.UtcNow, false));
+    var ip = request.HttpContext.Connection.RemoteIpAddress?.ToString() ?? "127.0.0.1";
+    callbacks.Add(new(Guid.NewGuid(), content, ip, DateTime.UtcNow, false));
     return Results.Text("Ok");
 });
 
@@ -32,17 +24,18 @@ app.MapGet("/callbacks", () =>
 
 app.MapGet("/", () =>
 {
-    var html = new StringBuilder("<html><head><title>Callbacks</title></head><body>");
-    html.AppendLine("<style>html {font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;}</style>");
-    html.AppendLine("<style>body {display: flex; line-height: 1.5; background-color: #1d1934; color: #f6f6f6; margin: 0; padding: 0 }</style>");
-    html.AppendLine("<style> .enviado { background: #00be36ff;} .pendente { background: #bd9700ff;} </style>");
-    html.AppendLine("<div style='width:20%; height: 100%; position: fixed; overflow: auto; border-right: 2px solid #2f2a6dff;'>");
+    var html = new StringBuilder("<html><head><title>Simple Webhook</title></head><body>");
+    html.AppendLine("<style>html { font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, sans-serif; } </style>");
+    html.AppendLine("<style>body { display: flex; line-height: 1.5; background-color: #1d1934; color: #f6f6f6; margin: 0; padding: 0; }</style>");
+    html.AppendLine("<style> .enviado { background: #339900;} .pendente { background: #ff9966;} </style>");
+    html.AppendLine("<div style='width: 300px; height: 100%; position: fixed; overflow: auto; border-right: 2px solid #2f2a6dff; background: #1d1934;'>");
     foreach (var callback in callbacks.OrderByDescending(x => x.CreatedAt))
     {
         var status = callback.Sended ? "enviado" : "pendente";
-        html.AppendLine($"<div style='padding: 10px; margin: 15px 10px; cursor: pointer;' class='{status}' onclick='show(\"{callback.Id}\")'>{callback.CreatedAt.ToString("dd/MM/yyyy hh:mm:ss")} ({status})</div>");
+        html.AppendLine($"<div style='padding: 10px; margin: 15px 10px; cursor: pointer;' class='{status}' onclick='show(\"{callback.Id}\")'>");
+        html.AppendLine($"{callback.Ip}<br/>{callback.CreatedAt.AddHours(-3).ToString("dd/MM/yyyy hh:mm:ss")}</div>");
     }
-    html.AppendLine("</div><div style='width:80%; padding-left: 20%'>");
+    html.AppendLine("</div><div style='width: calc(100% - 300px); padding-left: 300px'>");
     foreach (var callback in callbacks.OrderByDescending(x => x.CreatedAt))
     {
         html.AppendLine($"<div style='padding: 10px 20px; display: none' id={callback.Id} class='content'><pre><code>{callback.Content}</code></pre></div>");
@@ -54,19 +47,11 @@ app.MapGet("/", () =>
 
 app.Run();
 
-class Callback(Guid id, string content, DateTime createdAt, bool sended)
+class Callback(Guid id, string content, string ip, DateTime createdAt, bool sended)
 {
     public Guid Id { get; } = id;
     public string Content { get; } = content;
+    public string Ip { get; } = ip;
     public DateTime CreatedAt { get; } = createdAt;
     public bool Sended { get; set; } = sended;
-}
-
-[JsonSerializable(typeof(string))]
-[JsonSerializable(typeof(Callback))]
-[JsonSerializable(typeof(Callback[]))]
-[JsonSerializable(typeof(List<Callback>))]
-internal partial class AppJsonSerializerContext : JsonSerializerContext
-{
-
 }
