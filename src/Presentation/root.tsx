c5b636661d8fcc2386dd4484ca04e60af81b9f07
Jeziel Moura <jezielmoura@outlook.com>
2025-09-29 08:53:02 -0300

    Add favicon

--- src/Presentation/root.tsx
+++ src/Presentation/root.tsx
@@ -14,7 +14,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="shortcut icon" href="/logo.png" type="image/png" /> 
+        <link rel="shortcut icon" href="/icon.png" type="image/png" /> 
         <Meta />
         <Links />
       </head>
